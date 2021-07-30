603de650a25b34a73835af41e381d9f6d9806bba
TharunKumar999 <85543591+TharunKumar999@users.noreply.github.com>
2021-07-30 13:09:29 +0530

    Add files via upload

---
+++ junit_assertions_demo/src/main/java/com/example/junit_assertions_demo/Calculator.java
@@ -0,0 +1,18 @@
+package com.example.junit_assertions_demo;
+
+public class Calculator {
+	
+	public Integer addition(Integer firstNumber,Integer secondNumber)
+	{
+		return firstNumber+secondNumber;
+	}
+
+
+	public int sub(int a,int b)
+	{
+		a=100;
+		b=20;
+		return a-b;
+	}
+}
+	
